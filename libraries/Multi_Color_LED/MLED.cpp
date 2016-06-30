b78f2c4a8080fbf39719645ec516ef92e2b44ae7
ThatOneRuffian <Mestamarcus@yahoo.com>
2016-06-29 19:55:02 -0700

    Updated LED libraries
    LED are now passed via reference to make faster and less bulky code.
    Clearcolors no longer needs to be used to clear the display color
    functions will take care of that now.

--- libraries/Multi_Color_LED/MLED.cpp
+++ libraries/Multi_Color_LED/MLED.cpp
@@ -5,77 +5,100 @@
 */
 
 #include "MLED.h"
-int MLED::RED, MLED::BLUE, MLED::GREEN, MLED::BRIGHTNESS;
+int MLED::RED, MLED::BLUE, MLED::GREEN, MLED::ENABLE;
 
-MLED::MLED(int Red, int Blue, int Green, int Brightness){
+MLED::MLED(int& Red, int& Blue, int& Green, int& Enable){
 
-this -> BRIGHTNESS = Brightness;
-this -> RED = Red;
+this -> ENABLE = Enable;
+this -> RED = Red; //Upon creating the object the proper pins are setup for you.
 this -> BLUE = Blue;
 this -> GREEN = Green;
 
-pinMode(BRIGHTNESS, OUTPUT);
+pinMode(ENABLE, OUTPUT);
 pinMode(RED, OUTPUT);                //sets up pins to be output pins.
 pinMode(BLUE, OUTPUT);
 pinMode(GREEN, OUTPUT);
 
-digitalWrite(BRIGHTNESS, HIGH);
+pinMode(ENABLE, HIGH);                 // Output enabled
+analogWrite(ENABLE, 255);           //default full Brightness
 digitalWrite(RED, HIGH);            //Default mode off. HIGH is off for this particular LED
 digitalWrite(BLUE, HIGH);
 digitalWrite(GREEN, HIGH);
 
 }
 
-void MLED::SetBrightness(int Val){
+void MLED::SetBrightness(int& Val){
 
 if(Val >= 0 && Val <= 255)
-{
-   analogWrite( this -> BRIGHTNESS, Val);
-}
+{ analogWrite( this -> ENABLE, Val);}
 
 }
 
 void MLED::Purple(void){
-Blue();
-Red();
+   this -> ClearColors();
+   digitalWrite(this -> BLUE, LOW);
+   digitalWrite(this -> RED, LOW);
 }
 
 void MLED::Yellow(void){
-
-  Green();
-  Red();
+   this -> ClearColors();
+   digitalWrite(this -> GREEN, LOW);
+   digitalWrite(this -> RED, LOW);
 }
 
 void MLED::LightBlue(void){
-
-  Green();
-  Blue();
+   this -> ClearColors();
+   digitalWrite(this -> GREEN, LOW);
+   digitalWrite(this -> BLUE, LOW);
 }
 
 void MLED::White(void){
-
-  Green();
-  Blue();
-  Red();
+   this -> ClearColors();
+   digitalWrite(this -> GREEN, LOW);
+   digitalWrite(this -> BLUE, LOW);
+   digitalWrite(this -> RED, LOW);
 }
 
 void MLED::Green(void){
-
-  digitalWrite(GREEN, LOW);
+  this -> ClearColors();
+  digitalWrite(this -> GREEN, LOW);
 }
 
 void MLED::Blue(void){
-
-  digitalWrite(BLUE, LOW);
+  this -> ClearColors();
+  digitalWrite(this -> BLUE, LOW);
 }
 
 void MLED::Red(void){
+  this -> ClearColors();
+  digitalWrite(this -> RED, LOW);
+}
 
-  digitalWrite(RED, LOW);
+void MLED::Fade(double& FadeCycleTime)
+{
+    /*
+    const double MaxVal = 255; //Max PWM speed for arduino.
+
+    double i; // while loop to delay fade
+
+    if (FadeCycleTime > 0)
+    {
+        const double PeakTime = (FadeCycleTime/2.0); //time required to reach MaxVal
+
+
+        while()
+        {
+
+       SetBrightness(i);
+        delay()
+        }
+    }
+*/
+
 }
 
 void MLED::ClearColors(void){
-digitalWrite(RED,HIGH);
-digitalWrite(BLUE,HIGH);
-digitalWrite(GREEN,HIGH);
+digitalWrite(this -> RED,HIGH);
+digitalWrite(this -> BLUE,HIGH);
+digitalWrite(this -> GREEN,HIGH);
 }
